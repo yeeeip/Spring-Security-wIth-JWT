ff4917f808fdc10c5fa49440e894963dc82c6578
SHISH <dmit.nujdin2014@yandex.ru>
2023-01-29 14:47:17 +0400

    Created User and Role classes

---
+++ src/main/java/com/nuzhd/springsecurityjwt/user/MyUser.java
@@ -0,0 +1,69 @@
+package com.nuzhd.springsecurityjwt.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "my_user")
+public class MyUser implements UserDetails {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/main/java/com/nuzhd/springsecurityjwt/user/Role.java
@@ -0,0 +1,6 @@
+package com.nuzhd.springsecurityjwt.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
